3d30d51992ad63ffa31ccb49718ce6b2ea396474
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2023-03-01 22:51:45 +0300

    task4

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean run = true;
+        LinkedList<String> list = new LinkedList<String>();
+        while(run){
+            String[] split = in.nextLine().split("~");
+            String word = split[0];
+            String numberString = split[1];
+            
+            if (word.equals("print")) {
+               for (int i = 0; i < list.size(); i++) {
+                    if (numberString.equals(list.get(i))) { 
+                        System.out.println(list.get(i+1));      
+                    }
+                }
+            }
+            else if (word.equals("exit")){
+            run = false;
+            }      
+            else{
+                list.add(numberString);
+                list.add(word);
+            }
+        
+    }
+    in.close();
+}
+}
